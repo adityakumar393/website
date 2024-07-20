f56769f56d05e63f509eb7381b830a9326bfdecd
Aditya Kumar <159869878+adityakumar393@users.noreply.github.com>
2024-07-20 17:24:07 +0530

    Update formhandeler.php

--- formhandeler.php
+++ formhandeler.php
@@ -4,7 +4,7 @@ $visitor_email= $_POST['email'];
 $subject= $_POST['subject'];
 $message= $_POST['message'];
 
-$email_from ='adityakr421742gmail.com'; 
+$email_from ='https://adityakumar393.github.io/website/Contact.html'; 
 $email_subject = 'New Form Submission';
 $email_body = "User Name: $name.\n".
                 "User Email: $visitor_email.\n".
